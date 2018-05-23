7730fad86920f7f73e42876f0f24a4759c0bf462
mickkky <641401353@qq.com>
2018-05-23 22:36:07 +0800

    V1

--- fiteclipse.py
+++ fiteclipse.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 import numpy as np
 import cv2
@@ -17,42 +16,75 @@ def findeClipseContours(contours):
 # M=cv2.moments(cnt)
 # print(M)
 
-img = cv2.imread('/Users/wangbenkang/Desktop/test.jpg')
+protoimg = cv2.imread('/home/wangbenkang/桌面/test1.jpg')
+
+X1 = 1111
+Y1 = 1593
+
+X2 = 1208
+Y2 = 1700
+
+W = abs(X1-X2)
+H = abs(Y1-Y2)
+
+img = protoimg[Y1:Y1+H, X1:X1+W]
 imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-part = imgray[0: 100, 50: 100]
-cv2.imshow('center point', part)
+
+houghcircles = cv2.HoughCircles(imgray, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
+# for i in houghcircles[0,:]:
+#     # draw the outer circle
+#     cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
+#     # draw the center of the circle
+#     cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
+
 #找出图片中的轮廓
 ret,thresh = cv2.threshold(imgray,127,255,0)
-image ,contours,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_TC89_L1)
+image , contours,hierarchy = cv2.findContours(thresh,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
 #绘制独立轮廓，如第四个轮廓
 #imag = cv2.drawContour(img,contours,-1,(0,255,0),3)
 #但是大多数时候，下面方法更有用
-# imag = cv2.drawContours(img,contours,findeClipseContours(contours),(0,255,0),3)
+# imag = cv2.drawContours(img,contours,1,(0,255,0),3)
 # findeClipseContours(contours)
 # 椭圆拟合
-cnt = contours[findeClipseContours(contours)]
-ellipse = cv2.fitEllipse(cnt)
-eclipseimg = cv2.ellipse(img,ellipse,(55,255,155),2)
+circlescenter_list = []
+ellipse_list = []
+for i in range(len(contours) - 1):
+
+    if(len(contours[i])<6):
+        continue
+
+    cnt = contours[i]
+    ellipse = cv2.fitEllipse(cnt)
+    distance = pow((ellipse[0][0]-houghcircles[0,0][0]),2)+pow((ellipse[0][1]-houghcircles[0,0][1]),2)
+    circlescenter_list.append(distance)
+    ellipse_list.append(ellipse)
+
+Index = circlescenter_list.index(min(circlescenter_list))  # 圆心距最小值
+eclipseimg = cv2.ellipse(img, ellipse_list[Index], (55, 255, 155), 2)
+
+# cnt = contours[3]
+# ellipse = cv2.fitEllipse(cnt)
+# eclipseimg = cv2.ellipse(img,ellipse,(55,255,155),2)
 
 # 画出圆心（拟合出的圆心的亚像素级的，画出圆心需像素级，所以需要一次整型转换！）
-centerpoint = (int(ellipse[0][0]),int(ellipse[0][1]))
+centerpoint = (X1+int(ellipse_list[Index][0][0]),Y1+int(ellipse_list[Index][0][1]))
 print(centerpoint)
-eclipsecenter = cv2.circle(img, centerpoint, 2, (0, 0, 255),-1)
+eclipsecenter = cv2.circle(protoimg, centerpoint, 2, (0, 0, 255),-1)
 
 # 文字显示圆心坐标
 font = cv2.FONT_HERSHEY_SIMPLEX
-text = 'center:' + str(ellipse[0])
-cv2.putText(img, text, centerpoint, font,0.5, (255, 255, 0), 1, lineType=cv2.LINE_AA)
+text = 'center:' + str(ellipse_list[Index][0])
+cv2.putText(protoimg, text, centerpoint, font,0.5, (255, 255, 0), 1, lineType=cv2.LINE_AA)
 
 while(1):
     # cv2.imshow('img',img)
     # cv2.imshow('imgray',imgray)
     # cv2.imshow('image',image)
-    # cv2.imshow('imag',imag)
-    cv2.imshow('eclipse',eclipseimg)
-    cv2.imshow('center point', eclipsecenter)
+    cv2.namedWindow('protoimg',cv2.WINDOW_NORMAL)
+    cv2.imshow('protoimg', protoimg)
+
+    cv2.waitKey(0)
     if cv2.waitKey(1) == ord('q'):
         break
 cv2.destroyAllWindows()
-
